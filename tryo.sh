bd2088261243c7b5aa9f113e9a2c412b579b6e37
ola bam <ola@ip-172-31-54-101.ec2.internal>
2023-03-04 05:33:22 +0000

    modified

--- tryo.sh
+++ tryo.sh
@@ -1,3 +1,5 @@
 I will make it with this devOps by his grace. Inshallah
 make we sha dey pray say make anything for this new governemnt not hurt us.
 Make we dey pray for something good.
+greate
+make mi runa am
